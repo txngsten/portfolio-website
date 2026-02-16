7d55157a260e6049dd6e57831a0946ad598dee2a
wutt0019 <wutt0019@flinders.edu.au>
2026-02-16 12:04:42 +1030

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,7 @@ import { defineConfig, envField } from "astro/config";
 export default defineConfig({
   output: "static",
   site: "https://txngsten.github.io",
-  base: "/portfolio-website", // remove if repo is username.github.io
+  base: "/portfolio-website",
 
   integrations: [react()],
-
-  env: {
-    schema: {
-      CLIENT_ID: envField.string({ context: "server", access: "secret" }),
-      CLIENT_SECRET: envField.string({ context: "server", access: "secret" }),
-      REFRESH_TOKEN: envField.string({ context: "server", access: "secret" }),
-    },
-  },
 });
